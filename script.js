594db32a4ef54bc1f10dbccee79327f4e24a0f39
Ramla-Razykan <ramla.eman.ra@gmail.com>
2025-01-05 23:19:58 +0500

    complete

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// Slide Function
-let currentSlide = 0;
-function showSlide(index) {
-  const slides = document.querySelectorAll(".worksSlider .workcard ");
-  slides[currentSlide].classList.remove("active");
-  currentSlide = (index + slides.length) % slides.length;
-  slides[currentSlide].classList.add("active");
-}
-
-function nextSlide() {
-  showSlide(currentSlide + 1);
-}
-
-function prevSlide() {
-  showSlide(currentSlide - 1);
-}
-
-
 
 // Download CV functionality
 function downloadCV() {
